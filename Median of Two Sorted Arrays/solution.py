7b59ba9cb3e682e2aef9d5aa80d70d78e5e89f84
fangniu <838307912@qq.com>
2017-07-09 21:21:02 +0800

    add question

---
+++ Median of Two Sorted Arrays/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+
+if __name__ == '__main__':
+    print Solution().findMedianSortedArrays([1, 5], [2, 4])
